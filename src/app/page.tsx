88e1420f52a351f531d9399ef09ac7b1deebbd91
Matheus Rocha <matheus.rocha89@gmail.com>
2024-12-29 11:03:20 +0000

    Removing h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ import { UrlForm } from "@/components/UrlForm";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Link Chop</h1>
+    <main className="py-4">
       <UrlForm />
-    </div>
+    </main>
   );
 }
